715f7afc2b94adbd641f432b6283df0578d83814
Alice Hsing <alicehsing@gmail.com>
2022-05-09 10:34:54 -0700

    refactored mock data into its own files

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,78 +5,20 @@ import { rest } from 'msw';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
 import { UserProvider } from './context/UserContext';
+import { mockEntriesData } from './fixtures/mockEntriesData';
+import { mockAuthData } from './fixtures/mockAuthData';
 
 const server = setupServer(
   //mock out endpoint that does authentication
   rest.post(
     'https://ezwbsacoojmonmiqffad.supabase.co/auth/v1/token',
-    (req, res, ctx) =>
-      res(
-        ctx.json({
-          access_token: 'MOCKED_ACCESS_TOKEN',
-          token_type: 'bearer',
-          expires_in: 3600,
-          refresh_token: 'MOCKED_REFRESH_TOKEN',
-          user: {
-            id: '123456',
-            aud: 'authenticated',
-            role: 'authenticated',
-            email: 'testuser3@test.com',
-            email_confirmed_at: '2022-05-08T06:38:10.115918Z',
-            phone: '',
-            confirmed_at: '2022-05-08T06:38:10.115918Z',
-            last_sign_in_at: '2022-05-08T06:41:41.015688272Z',
-            app_metadata: {
-              provider: 'email',
-              providers: ['email'],
-            },
-            user_metadata: {},
-            identities: [
-              {
-                id: '123456',
-                user_id: '123456',
-                identity_data: {
-                  sub: '123456',
-                },
-                provider: 'email',
-                last_sign_in_at: '2022-05-08T06:38:10.114152Z',
-                created_at: '2022-05-08T06:38:10.114199Z',
-                updated_at: '2022-05-08T06:38:10.114203Z',
-              },
-            ],
-            created_at: '2022-05-08T06:38:10.104387Z',
-            updated_at: '2022-05-08T06:41:41.016734Z',
-          },
-        })
-      )
+    (req, res, ctx) => res(ctx.json(mockAuthData))
   ),
 
   // mock out endpoint that fetches entries
   rest.get(
     'https://ezwbsacoojmonmiqffad.supabase.co/rest/v1/entries',
-    (req, res, ctx) =>
-      res(
-        ctx.json([
-          {
-            id: 419,
-            guest_id: '123456',
-            content: "Can't wait for summer to start!",
-            created_at: '2022-05-08T06:52:02.76014+00:00',
-          },
-          {
-            id: 418,
-            guest_id: '123456',
-            content: 'I want some sun please!!!\n',
-            created_at: '2022-05-08T06:51:40.701669+00:00',
-          },
-          {
-            id: 417,
-            guest_id: '123456',
-            content: 'Hello World!',
-            created_at: '2022-05-08T06:51:12.555836+00:00',
-          },
-        ])
-      )
+    (req, res, ctx) => res(ctx.json(mockEntriesData))
   )
 );
 
